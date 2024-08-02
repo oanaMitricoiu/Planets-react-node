6a40111c489f462c9ad68c38562d0dd4eb605c8c
oana-iu <oana.mitri01@gmail.com>
2024-08-02 10:18:08 +0300

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const app = require("./app");
+
+const { loadPlanetsData } = require("./models/planets.model");
+
+const PORT = 8000;
+
+const server = http.createServer(app);
+
+async function startServer() {
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+}
+
+startServer();
